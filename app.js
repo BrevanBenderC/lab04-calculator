28cb229333204ae949aa7e8c09712e7fa769e46e
Brevan Bender <bcaybender@gmail.com>
2021-09-30 13:49:57 -0700

    finished testing tabs and started on addition

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 // import functions and grab DOM elements
+const input1 = document.getElementById('firstInput');
+const input2 = document.getElementById('secondInput');
+const button = document.getElementById('compute')
+const answeradd = document.getElementById('answerAdd')
+
+
+button.addEventListener ('click', ()=>{
+    //let final = (input1 + input2);
+    //answeradd.numberContent = final;
+    function addinputs(input1)
+    
+
+});
 
 // initialize global state
 
